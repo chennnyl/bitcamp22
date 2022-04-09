0c9a6ad406f3506724c4fa51f2ec5608113aa414
Lynne Cure <lynne@lynnux.org>
2022-04-08 22:43:31 -0400

    vector header

--- include/vec2.h
+++ include/vec2.h
@@ -7,11 +7,21 @@
 extern "C" {
 #endif
 
+typedef s32 fixed24;
+
 typedef struct Vector2 {
-    s32 x;
-    s32 y;
+    fixed24 x;
+    fixed24 y;
 } Vector2;
 
+Vector2 vec2_add(Vector2 *v1, Vector2 *v2);
+Vector2 vec2_sub(Vector2 *v1, Vector2 *v2);
+Vector2 vec2_scale(Vector2 *vec, fixed24 scalar);
+
+Vector2 vec2_norm(Vector2 *vec);
+fixed24 vec2_sqrmag(Vector2 *vec);
+fixed24 vec2_mag(Vector2 *vec);
+fixed24 vec2_dot(Vector2 *v1, Vector2 *v2);
 
 
 #ifndef __cplusplus
