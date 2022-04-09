f63f8b333f9a5638ebba7a71cda74e4998ada9ee
gIsForGravity <37271917+gIsForGravity@users.noreply.github.com>
2022-04-08 23:47:44 -0400

    renamed fixed24 to fixed32

--- include/vec2.h
+++ include/vec2.h
@@ -7,21 +7,21 @@
 extern "C" {
 #endif
 
-typedef s32 fixed24;
+typedef s32 fixed32;
 
 typedef struct Vector2 {
-    fixed24 x;
-    fixed24 y;
+    fixed32 x;
+    fixed32 y;
 } Vector2;
 
 Vector2 vec2_add(Vector2 v1, Vector2 v2);
 Vector2 vec2_sub(Vector2 v1, Vector2 v2);
-Vector2 vec2_scale(Vector2 vec, fixed24 scalar);
+Vector2 vec2_scale(Vector2 vec, fixed32 scalar);
 
 Vector2 vec2_norm(Vector2 vec);
-fixed24 vec2_sqrmag(Vector2 vec);
-fixed24 vec2_mag(Vector2 vec);
-fixed24 vec2_dot(Vector2 v1, Vector2 v2);
+fixed32 vec2_sqrmag(Vector2 vec);
+fixed32 vec2_mag(Vector2 vec);
+fixed32 vec2_dot(Vector2 v1, Vector2 v2);
 
 
 #ifdef __cplusplus
