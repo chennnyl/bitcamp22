f7f74743f7db2f6caa4d84f4e2588d18bf180435
gIsForGravity <37271917+gIsForGravity@users.noreply.github.com>
2022-04-09 11:16:55 -0400

    Create util.h

---
+++ include/util.h
@@ -0,0 +1,25 @@
+/**
+ * @file util.h
+ * @author gavint
+ * @brief utils and stuff
+ * @version 0.1
+ * @date 2022-04-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#define WHERE fprintf(stderr,"[LOG] See %s:%d\n",__FILE__,__LINE__);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
